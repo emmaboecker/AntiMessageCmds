519c825c45a7cdb7f81c53761dd68675fa0cb2e2
StckOverflw <stckoverflw@gmail.com>
2022-01-30 16:05:56 +0100

    Add Ignore case Regex option

--- src/main/kotlin/net/stckoverflw/antimessagecmds/config/Config.kt
+++ src/main/kotlin/net/stckoverflw/antimessagecmds/config/Config.kt
@@ -5,7 +5,7 @@ import dev.schlaubi.mikbot.plugin.api.EnvironmentConfig
 object Config : EnvironmentConfig("") {
 
     val MESSAGE_REGEX: Regex by getEnv {
-        it.toRegex()
+        it.toRegex(RegexOption.IGNORE_CASE)
     }
 
     val RESPONSE_OVERRIDE: String? by environment.optional()
